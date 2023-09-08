fe6402ff36098bbb6722cba231e00739020980c8
Tevin79 <wtevin2015@gmail.com>
2023-09-08 09:23:34 +0200

    buttons edt

--- edt/script.js
+++ edt/script.js
@@ -1,19 +1,40 @@
 function l2_info() {
     document.getElementById("l2_info").style.display = "flex";
+    document.getElementById("l2-info-button").classList.add("active");
     document.getElementById("l3_info").style.display = "none";
+    if (document.getElementById("l3-info-button").classList.contains("active")){
+        document.getElementById("l3-info-button").classList.remove("active");
+    }
     document.getElementById("l3_maths").style.display = "none";
+    if (document.getElementById("l3-maths-button").classList.contains("active")){
+        document.getElementById("l3-maths-button").classList.remove("active");
+    }
 }
 
 function l3_info() {
     document.getElementById("l2_info").style.display = "none";
+    if (document.getElementById("l2-info-button").classList.contains("active")){
+        document.getElementById("l2-info-button").classList.remove("active");
+    }
     document.getElementById("l3_info").style.display = "flex";
+    document.getElementById("l3-info-button").classList.add("active");
     document.getElementById("l3_maths").style.display = "none";
+    if (document.getElementById("l3-maths-button").classList.contains("active")){
+        document.getElementById("l3-maths-button").classList.remove("active");
+    }
 }
 
 function l3_maths() {
     document.getElementById("l2_info").style.display = "none";
+    if (document.getElementById("l2-info-button").classList.contains("active")){
+        document.getElementById("l2-info-button").classList.remove("active");
+    }
     document.getElementById("l3_info").style.display = "none";
+    if (document.getElementById("l3-info-button").classList.contains("active")){
+        document.getElementById("l3-info-button").classList.remove("active");
+    }
     document.getElementById("l3_maths").style.display = "flex";
+    document.getElementById("l3-maths-button").classList.add("active");
 }
 
 function load() {
