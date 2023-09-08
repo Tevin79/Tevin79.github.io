5883fc203b5108b79357302745c3109cfc480c7a
Tevin79 <wtevin2015@gmail.com>
2023-09-08 13:24:24 +0200

    responsive fix

--- liens/script.js
+++ liens/script.js
@@ -157,6 +157,10 @@ if (screen.height > screen.width){
 	document.getElementById("style").setAttribute("href", "../styleblue_small.css");
 }
 
+if (screen.height > screen.width){
+	document.getElementById("style_page").setAttribute("href", "style_small.css");
+}
+
     //document.write("</div>");
 
 
--- pbm_box/script.js
+++ pbm_box/script.js
@@ -1,3 +1,7 @@
 if (screen.height > screen.width){
     document.getElementById("style").setAttribute("href", "../styleblue_small.css");
 }
+
+if (screen.height > screen.width){
+    document.getElementById("style_page").setAttribute("href", "style_small.css");
+}
